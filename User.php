1d52c8a812a211ee353074d7ef981d525a23aa47
Valentin Le Bon <lebonvalentin@hotmail.fr>
2020-07-15 17:51:24 +0200

    Starting TP 2, adding DockerFile and begining of class User

---
+++ User.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class User
+{
+    private $email;
+    private $firstName;
+    private $lastName;
+    private $age;
+
+    public function __construct(string $email,string $firstName, string $lastName, int $age)
+    {
+        
+    }
+}
